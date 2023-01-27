c0d545756091ffff09355100287bc3c297d13a7c
carlosDiaz <diaz-alzate@hotmail.com>
2023-01-26 22:04:42 -0500

    partners practice

---
+++ database/factories/RegistroFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Registro>
+ */
+class RegistroFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'nombre'=>fake()->name(),
+            'cedula'=>fake()->unique()->numberBetween(1111111,9999999),
+            //
+        ];
+    }
+}
